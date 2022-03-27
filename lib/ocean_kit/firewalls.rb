958627a0e87bb21c01b109af4a0fc656089ba9fe
Leo Policastro <leo.policastro@hey.com>
2022-03-27 16:44:20 -0400

    clean up

--- lib/ocean_kit/firewalls.rb
+++ lib/ocean_kit/firewalls.rb
@@ -7,6 +7,9 @@ module OceanKit
       puts pastel.white.bold.underline("Firewalls:\n")
       do_client.firewalls.all.each_with_index do |firewall, index|
         puts pastel.white.bold("[#{index}]: Firewall #{firewall.name} has #{firewall.droplet_ids.count} droplets")
+        firewall_inbound_rules(firewall).each_with_index do |rule, ii|
+          puts pastel.blue.bold("     [#{ii}] #{pastel.blue.bold(rule)}")
+        end
       end
     end
 
@@ -14,12 +17,11 @@ module OceanKit
     def enable_all_ssh
       puts pastel.white.bold("Enabling SSH on all firewalls")
       do_client.firewalls.all.each_with_index do |fw, index|
-        firewall = do_client.firewalls.find(id: fw.id)
-        inbound_rules = firewall_inbound_rules(firewall)
-        firewall.inbound_rules = add_ssh_rule(inbound_rules)
+        inbound_rules = firewall_inbound_rules(fw)
+        fw.inbound_rules = add_ssh_rule(inbound_rules)
         begin
-          update_firewall(firewall)
-          puts pastel.green.bold("SSH enabled on firewall #{firewall.name}")
+          update_firewall(fw)
+          puts pastel.green.bold("SSH enabled on firewall #{fw.name}")
         rescue DropletKit::Error => e
           puts pastel.red.bold("Error: #{e.message}")
         end
@@ -30,12 +32,11 @@ module OceanKit
     def disable_all_ssh
       puts pastel.white.bold("Disabling SSH on all firewalls")
       do_client.firewalls.all.each_with_index do |fw, index|
-        firewall = do_client.firewalls.find(id: fw.id)
-        inbound_rules = firewall_inbound_rules(firewall)
-        firewall.inbound_rules = remove_ssh_rule(inbound_rules)
+        inbound_rules = firewall_inbound_rules(fw)
+        fw.inbound_rules = remove_ssh_rule(inbound_rules)
         begin
-          update_firewall(firewall)
-          puts pastel.green.bold("SSH disabled on firewall #{firewall.name}")
+          update_firewall(fw)
+          puts pastel.green.bold("SSH disabled on firewall #{fw.name}")
         rescue DropletKit::Error => e
           puts pastel.red.bold("Error: #{e.message}")
         end
