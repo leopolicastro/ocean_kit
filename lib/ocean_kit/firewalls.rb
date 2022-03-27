03e600910d936b56d50fb145fa668fd84c0984e3
Leo Policastro <leo.policastro@hey.com>
2022-03-27 12:58:13 -0400

    update list firewalls method, add helpers

--- lib/ocean_kit/firewalls.rb
+++ lib/ocean_kit/firewalls.rb
@@ -7,11 +7,29 @@ module OceanKit
     desc "list", "Lists all firewalls"
     def list
       puts "List all firewalls"
-      @digital_ocean = OceanKit::OceanClient.new
-      @digital_ocean.client.firewalls.all.each do |firewall|
-        puts "Firewall #{firewall.name} has #{firewall.droplet_ids.count} droplets"
-        puts firewall.id
+
+      do_client.firewalls.all.each_with_index do |firewall, index|
+        puts "[#{index}]: Firewall #{firewall.name} has #{firewall.droplet_ids.count} droplets"
       end
+    end
+
+    private
+
+    def disable_ssh(rules_array)
+      rules_array.delete_if { |r| r[:ports] == "22" }
+    end
+
+    def do_client
+      @digital_ocean = OceanKit::OceanClient.new
+      @digital_ocean.client
+    end
+
+    def new_inbound_rule(rule)
+      DropletKit::FirewallInboundRule.new(
+        protocol: rule[:protocol],
+        ports: rule[:ports],
+        sources: rule[:sources]
+      )
     end
   end
 end
